00c8e8919185b44618a66de9592e10c465f4dce7
ww8007 <wshmin@naver.com>
2021-01-06 16:15:20 +0900

    information change

--- 생활코딩/react-todo/src/App.js
+++ 생활코딩/react-todo/src/App.js
@@ -1,55 +1,56 @@
-import "./App.css";
-import React, { Component } from "react";
+import './App.css';
+import TOC from './components/TOC';
+import Article from './components/Article';
+import Subject from './components/Subject';
+import React, { Component } from 'react';
 
-class Subject extends Component {
-   render() {
-      return (
-         <header>
-            <h1>WEB</h1>
-            world wide web!
-         </header>
-      );
-   }
-}
-class TOC extends Component {
-   render() {
-      return (
-         <nav>
-            <ul>
-               <li>
-                  <a href="1.html">HTML</a>
-               </li>
-               <li>
-                  <a href="2.html">CSS</a>
-               </li>
-               <li>
-                  <a href="3.html">Java</a>
-               </li>
-            </ul>
-         </nav>
-      );
-   }
-}
-class Article extends Component {
-   render() {
-      return (
-         <article>
-            <h2>HTML</h2>
-            HTML is HyperText Markup Language.
-         </article>
-      );
-   }
-}
 class App extends Component {
-   render() {
-      return (
-         <div className="App">
-            <Subject></Subject>
-            <TOC></TOC>
-            <Article></Article>
-         </div>
-      );
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'read',
+      welcom: { title: 'Welcome', desc: 'Hello, React!' },
+      subject: { title: 'WEB', sub: 'world wide web!' },
+      contents: [
+        { id: 1, titles: 'HTML', des: 'HTML is for information' },
+        { id: 2, titles: 'CSS', des: 'CSS is for desing' },
+        { id: 3, titles: 'JavaScript', des: 'JavaScript is for interactive' },
+      ],
+    };
+  }
+  render() {
+    console.log('App render');
+    var _title,
+      _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcom.title;
+      _desc = this.state.welcom.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].des;
+    }
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}
+        ></Subject>
+
+        <TOC
+          onChangePage={function () {
+            this.setState({ mode: 'read' });
+          }.bind(this)}
+          data={this.state.contents}
+        >
+          {' '}
+        </TOC>
+        <Article title={_title} desc={_desc}></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
