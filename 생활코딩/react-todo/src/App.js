79b5e6ad7c7056b2ceae98bc12e8cd88a676ee89
ww8007 <wshmin@naver.com>
2021-01-07 16:39:15 +0900

    항목 추가

--- 생활코딩/react-todo/src/App.js
+++ 생활코딩/react-todo/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import TOC from './components/TOC';
-import Article from './components/Article';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
 import React, { Component } from 'react';
+import Control from './components/Control';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id: 2,
       welcom: { title: 'Welcome', desc: 'Hello, React!' },
       subject: { title: 'WEB', sub: 'world wide web!' },
@@ -22,10 +25,12 @@ class App extends Component {
   render() {
     console.log('App render');
     var _title,
-      _desc = null;
+      _desc,
+      _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcom.title;
       _desc = this.state.welcom.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === 'read') {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -37,6 +42,25 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === 'create') {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            // add content to this.state.contents
+            console.log(_title, _desc);
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              titles: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+            });
+          }.bind(this)}
+        ></CreateContent>
+      );
     }
     return (
       <div className="App">
@@ -56,7 +80,14 @@ class App extends Component {
         >
           {' '}
         </TOC>
-        <Article title={_title} desc={_desc}></Article>
+        <Control
+          onChangeMode={function (_mode) {
+            this.setState({
+              mode: _mode,
+            });
+          }.bind(this)}
+        ></Control>
+        {_article}
       </div>
     );
   }
