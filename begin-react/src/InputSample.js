ad5c8110700f462c5cfccf7216400808a46ece49
ww8007 <wshmin@naver.com>
2021-01-08 13:56:23 +0900

    useRef with input

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
   const [input, setInput] = useState({
     name: '',
     nickname: '',
   });
+  const nameInput = useRef();
   const { name, nickname } = input;
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -18,10 +19,17 @@ function InputSample() {
       name: '',
       nickname: '',
     });
+    nameInput.current.focus();
   };
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input
         name="nickname"
         placeholder="닉네임"
