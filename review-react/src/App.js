69c8189b9057984c143ca77af379dbdb5d1f68b4
ww8007 <wshmin@naver.com>
2021-01-08 21:36:59 +0900

    react review files create

---
+++ review-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useRef } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState({
+    id: '',
+    pw: '',
+  });
+  const { id, pw } = input;
+  const onChange;
+  return <div></div>;
+}
+
+export default App;
