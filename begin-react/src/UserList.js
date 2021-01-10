cbb8e2879358f063dfb3ce2a32b395290ecba1d1
ww8007 <wshmin@naver.com>
2021-01-10 14:56:23 +0900

    default error change

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -38,7 +38,4 @@ function UserList({ user, onRemove, onToggle }) {
   );
 }
 
-export default React.memo(
-  UserList,
-  (prevProps, nextProps) => nextProps.users === prevProps.users,
-);
+export default React.memo(UserList);
