63b4318a58728b6063f4398deafbc5970170c6f1
ww8007 <wshmin@naver.com>
2021-01-10 13:32:21 +0900

    useEffect hook add

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
-  useEffect(() => {
-    console.log(user);
-  });
+
   return (
     <div>
       <b
