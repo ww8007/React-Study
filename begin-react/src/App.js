932ff69b94ded2c4c6398fafad48bbf80aba5e38
ww8007 <wshmin@naver.com>
2021-01-10 19:08:30 +0900

    useReducer change

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,6 +2,7 @@ import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import './App.css';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
+import useInputs from './useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 유저들 수 카운트 중');
@@ -9,7 +10,6 @@ function countActiveUsers(users) {
 }
 
 const initialState = {
-  inputs: { username: '', email: '' },
   users: [
     {
       id: 1,
@@ -34,14 +34,6 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT':
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value,
-        },
-      };
     case 'CREATE_USER':
       return {
         inputs: initialState.inputs,
@@ -66,18 +58,13 @@ function reducer(state, action) {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
+  const { username, email } = form;
   const nextId = useRef(4);
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value,
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -89,7 +76,8 @@ function App() {
       },
     });
     nextId.current += 1;
-  }, [username, email]);
+    reset();
+  }, [username, email, reset]);
 
   const onToggle = useCallback((id) => {
     dispatch({
