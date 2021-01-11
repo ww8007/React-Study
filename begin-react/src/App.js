76395a00d64224f6a10370e4f3707bb6fff05776
ww8007 <wshmin@naver.com>
2021-01-11 16:16:24 +0900

    change with Context API

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,7 @@
-import React, { useRef, useReducer, useMemo, useCallback } from 'react';
+import React, { useReducer, useMemo, createContext } from 'react';
 import './App.css';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
-import useInputs from './useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 유저들 수 카운트 중');
@@ -56,56 +55,20 @@ function reducer(state, action) {
   }
 }
 
+export const UserDispatch = createContext(null);
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [form, onChange, reset] = useInputs({
-    username: '',
-    email: '',
-  });
-  const { username, email } = form;
-  const nextId = useRef(4);
   const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email,
-      },
-    });
-    nextId.current += 1;
-    reset();
-  }, [username, email, reset]);
-
-  const onToggle = useCallback((id) => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id,
-    });
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id,
-    });
-  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList user={users} onToggle={onToggle} onRemove={onRemove}></UserList>
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList user={users}></UserList>
       <div>활성 사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   );
 }
 
