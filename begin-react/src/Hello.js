1705a6518b8f84fed41bb38676f2763d352ab4e9
ww8007 <wshmin@naver.com>
2021-01-11 17:57:47 +0900

    change class component

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function HelloWolrd({ color, name, isSpecial }) {
-  console.log();
-  return (
-    <div
-      style={{
-        color,
-      }}
-    >
-      <b>{isSpecial ? '스페셜' : '낫스페셜'}</b>
-      안녕하세요{name}
-    </div>
-  );
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음',
+  };
+  render() {
+    const { isSpecial, color, name } = this.props;
+    return (
+      <div style={color}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    );
+  }
 }
 
-HelloWolrd.defaultProps = {
-  name: '이름없음',
-};
-export default HelloWolrd;
+// function HelloWolrd({ color, name, isSpecial }) {
+//   console.log();
+//   return (
+//     <div
+//       style={{
+//         color,
+//       }}
+//     >
+//       <b>{isSpecial ? '스페셜' : '낫스페셜'}</b>
+//       안녕하세요{name}
+//     </div>
+//   );
+// }
+
+// Hello.defaultProps = {
+//   name: '이름없음',
+// };
+export default Hello;
