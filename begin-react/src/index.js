3cd1758cad15e86d2bc0d4bd30b5880ad833ea0f
ww8007 <wshmin@naver.com>
2021-01-11 17:57:59 +0900

    change class component

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
 
 // import Counter_reducer from './Counter_reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello name="react" isSpecial="true" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
