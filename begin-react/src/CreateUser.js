fac8bd43da22134501832d874cd06048d6ae7d4b
ww8007 <wshmin@naver.com>
2021-01-11 16:16:46 +0900

    change with Context API

--- begin-react/src/CreateUser.js
+++ begin-react/src/CreateUser.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { UserDispatch } from './App';
+import useInputs from './useInputs';
 
-function CreateUser({ username, email, onChange, onCreate }) {
+function CreateUser() {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
+  const dispatch = useContext(UserDispatch);
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
+    reset();
+    nextId.current += 1;
+  };
   return (
     <div>
       <input
