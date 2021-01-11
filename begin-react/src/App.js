e4a4d7ffda5089b1aa8e162c983a1101fba074cc
ww8007 <wshmin@naver.com>
2021-01-11 17:57:33 +0900

    add immer produce

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,6 +2,7 @@ import React, { useReducer, useMemo, createContext } from 'react';
 import './App.css';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 유저들 수 카운트 중');
@@ -34,22 +35,33 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    // return {
+    //   inputs: initialState.inputs,
+    //   users: state.users.concat(action.user),
+    // };
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user,
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.user.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.map((user) =>
+    //     user.id === action.id ? { ...user, active: !user.active } : user,
+    //   ),
+    // };
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.filter((user) => user.id !== action.id),
+    // };
     default:
       throw new Error('Error');
   }
