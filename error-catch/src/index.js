9095fb93ae8316d391c86b53c30e62c149e0d965
ww8007 <wshmin@naver.com>
2021-01-12 13:18:01 +0900

    add sentry api

--- error-catch/src/index.js
+++ error-catch/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
+Sentry.init({
+  dsn:
+    'https://f44aebb8d2a145beb687e819cd68d77d@o503496.ingest.sentry.io/5588717',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
