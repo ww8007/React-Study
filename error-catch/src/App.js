cdbe97123d17045380d541111f9341b24c4a3aae
ww8007 <wshmin@naver.com>
2021-01-12 12:48:56 +0900

    ERROR_example_react_files

---
+++ error-catch/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './User';
+
+function App() {
+  const user = {
+    id: 1,
+    username: 'hi',
+  };
+  return (
+    <div>
+      <User></User>
+      <b>hihihi</b>
+    </div>
+  );
+}
+
+export default App;
