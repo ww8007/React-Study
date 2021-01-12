b8d5fe4ee5d207037959adf2b3d4bbe8ef7818c2
ww8007 <wshmin@naver.com>
2021-01-12 13:17:46 +0900

    default file

--- error-catch/src/App.js
+++ error-catch/src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import User from './User';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
-  const user = {
-    id: 1,
-    username: 'hi',
-  };
-  return (
-    <div>
-      <User></User>
-      <b>hihihi</b>
-    </div>
-  );
+  return <User></User>;
 }
 
 export default App;
