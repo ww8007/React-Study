d289345464ea3e072ce5b7bc27ec777c9439d6e5
ww8007 <wshmin@naver.com>
2021-01-12 20:12:47 +0900

    add react-icons

--- styling-with-css-module/src/components/CheckBox.js
+++ styling-with-css-module/src/components/CheckBox.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import styles from './CheckBox.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function CheckBox({ children, checked, ...rest }) {
   return (
-    <div>
+    <div className={cx('checkbox')}>
       <label>
         <input type="checkbox" checked={checked} {...rest} />
-        <div>{checked ? '체크됨' : '체크 안됨'}</div>
+        <div className={cx('icon')}>
+          {checked ? (
+            <MdCheckBox className={cx('checked')} />
+          ) : (
+            <MdCheckBoxOutlineBlank />
+          )}
+        </div>
       </label>
       <span>{children}</span>
     </div>
