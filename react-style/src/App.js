d690e81b9a47aeac61c6d210882c009aaf43d445
ww8007 <wshmin@naver.com>
2021-01-12 18:43:47 +0900

    ...rest add

--- react-style/src/App.js
+++ react-style/src/App.js
@@ -9,6 +9,45 @@ function App() {
         <Button>BUTTON</Button>
         <Button size="small">BUTTON</Button>
       </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" color="gray" fullWidth>
+          BUTTON
+        </Button>
+        <Button
+          size="large"
+          color="pink"
+          fullWidth
+          onClick={() => {
+            console.log('click');
+          }}
+          onMouseMove={() => {
+            console.log('mouse move');
+          }}
+        >
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
