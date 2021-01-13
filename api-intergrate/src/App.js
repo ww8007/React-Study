d94946f9064e5a334ef455f46f2839807b33aeea
ww8007 <wshmin@naver.com>
2021-01-13 16:53:59 +0900

    User Asynces drive

--- api-intergrate/src/App.js
+++ api-intergrate/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 // import Users from './Users';
-import Users_Reducer from './Users_Reducer';
+import Users_Asyncs from './Users_Asyncs';
 
 function App() {
-  return <Users_Reducer></Users_Reducer>;
+  return <Users_Asyncs></Users_Asyncs>;
 }
 
 export default App;
