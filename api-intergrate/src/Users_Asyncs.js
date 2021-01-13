6abb2808137327069676f67e3d4a004fba15e1d7
ww8007 <wshmin@naver.com>
2021-01-13 17:56:14 +0900

    import useAsync and use

--- api-intergrate/src/Users_Asyncs.js
+++ api-intergrate/src/Users_Asyncs.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-template-curly-in-string */
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 async function getUsers() {
   const response = await axios.get(
@@ -11,13 +11,14 @@ async function getUsers() {
 }
 
 function Users_Reducer() {
-  const [state, refetch] = useAsync(getUsers, [], true);
   const [userId, setUserId] = useState(null);
-  const { loading, data: users, error } = state;
+  const { data: users, error, isLoading, reload, run } = useAsync({
+    deferFn: getUsers,
+  });
 
-  if (loading) return <div>로딩중...</div>;
+  if (isLoading) return <div>로딩중...</div>;
   if (error) return <diiv>에러발생</diiv>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
   return (
     <>
       <ul>
@@ -31,7 +32,7 @@ function Users_Reducer() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={reload}>다시 불러오기</button>
       {userId && <User id={userId}></User>}
     </>
   );
