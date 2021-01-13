d8bbc97e4c465994e3f3e4b4fbddc9e0c3f8d384
ww8007 <wshmin@naver.com>
2021-01-13 16:09:39 +0900

    repacktering

---
+++ api-intergrate/src/asyncReducer.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-template-curly-in-string */
+// loading , scuccess, error
+export function asyncReducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS': {
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    }
+    case 'ERROR': {
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    }
+    default:
+      throw new Error('Unhandled action type: ${action.type}');
+  }
+}
