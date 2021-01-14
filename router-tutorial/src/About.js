e266bc9b962a0c0b27d7532691fbfc457de8854c
ww8007 <wshmin@naver.com>
2021-01-14 15:57:14 +0900

    install qs and use

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
-function About() {
+import qs from 'qs';
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  console.log(query);
+  const detail = query.detail === 'true';
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리엑트 라우터 기초를 실습해보는 예제 프로젝트 입니다.</p>
+      {detail && <p>detail 값이 true 입니다.</p>}
     </div>
   );
 }
