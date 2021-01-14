64d1b3e1d1b89090a02cdca0d81d1439fb9a77bb
ww8007 <wshmin@naver.com>
2021-01-14 15:58:07 +0900

    sub router add

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,27 @@
-import { Route } from 'react-router-dom';
-
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Profiles from './Profiles';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+      </ul>
+      <hr></hr>
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+      <Route path="/profiles" component={Profiles} />
+    </div>
+  );
 }
 
 export default App;
