219d571e13cb8183af70a00d2cd9c6669126a0a9
ww8007 <wshmin@naver.com>
2021-01-14 18:18:27 +0900

    add Switch

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import HistorySample from './HistorySample';
 import Home from './Home';
@@ -21,10 +21,20 @@ function App() {
         </li>
       </ul>
       <hr></hr>
-      <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
