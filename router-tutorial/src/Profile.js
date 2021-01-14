c178a8e190f3d013a145964ba0e27632977e7508
ww8007 <wshmin@naver.com>
2021-01-14 15:59:06 +0900

    Profile list example

---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const profileData = {
+  Jang: {
+    name: '장동현',
+    description: 'Frontend Engineer',
+  },
+  homer: {
+    name: '호머 심슨',
+    description: '심슨 가족에 나오는 아빠 역할 케릭터',
+  },
+};
+function Profile({ match }) {
+  const { username } = match.params;
+  const profile = profileData[username];
+
+  if (!profile) {
+    return <div>존재하지 않는 사용자 입니다.</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username} ({profile.name})<p>{profile.description}</p>
+      </h3>
+    </div>
+  );
+}
+
+export default Profile;
