602b2b75c392d384e4768ad5365e5b562c2b624b
ww8007 <wshmin@naver.com>
2021-01-14 17:42:02 +0900

    go to home add

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Link } from 'react-router-dom';
 import About from './About';
+import HistorySample from './HistorySample';
 import Home from './Home';
 import Profiles from './Profiles';
 function App() {
@@ -15,11 +16,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필 목록</Link>
         </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
       </ul>
       <hr></hr>
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
