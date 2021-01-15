6f5763cddf9cf7dbbfcc9d07ad113957dae1fb97
ww8007 <wshmin@naver.com>
2021-01-15 22:42:45 +0900

    add UseDispatch

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 function CounterContainer() {
-  const { number, diff } = useSelector((state) => {});
-  return <Counter></Counter>;
+  const { number, diff } = useSelector((state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff,
+  }));
+  const dispatch = useDispatch();
+
+  const onIncrease = () => dispatch(increase());
+  const onDecrease = () => dispatch(decrease());
+  const onSetDiff = (diff) => dispatch(setDiff(diff));
+  return (
+    <Counter
+      number={number}
+      diff={diff}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      onSetDiff={onSetDiff}
+    ></Counter>
+  );
 }
 
 export default CounterContainer;
