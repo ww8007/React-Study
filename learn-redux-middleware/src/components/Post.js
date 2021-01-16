fb71ebfca0e2a86ebdb4aebadeafa241c20d0451
ww8007 <wshmin@naver.com>
2021-01-16 22:54:05 +0900

    Add title and body

--- learn-redux-middleware/src/components/Post.js
+++ learn-redux-middleware/src/components/Post.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function Post({ post }) {
-  const { title, boyd } = post;
-  return <div></div>;
+  const { title, body } = post;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
 }
 
 export default Post;
