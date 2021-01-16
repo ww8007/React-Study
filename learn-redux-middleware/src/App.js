804056da9bf3396cbe18c5afbb3fc8eb1478b722
ww8007 <wshmin@naver.com>
2021-01-16 20:14:32 +0900

    Import PostListContainer

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,7 +1,7 @@
-import CounterContainer from './containers/CounterContainer';
+import PostListContainer from './containers/PostListContainer';
 
 function App() {
-  return <CounterContainer></CounterContainer>;
+  return <PostListContainer></PostListContainer>;
 }
 
 export default App;
