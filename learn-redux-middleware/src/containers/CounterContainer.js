65df17b41a3b21d0dd808da380581c39b2fb1e06
ww8007 <wshmin@naver.com>
2021-01-16 17:03:33 +0900

    dispatch with increaseAsync, decreaseAsync

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Counter from '../components/Counter';
 import { useSelector, useDispatch } from 'react-redux';
-import { decrease, increase } from '../modules/counter';
+import { decreaseAsync, increaseAsync } from '../modules/counter';
 function CounterContainer() {
   const number = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const onIncrease = () => {
-    dispatch(increase());
+    dispatch(increaseAsync());
   };
   const onDecrase = () => {
-    dispatch(decrease());
+    dispatch(decreaseAsync());
   };
   return (
     <div>
