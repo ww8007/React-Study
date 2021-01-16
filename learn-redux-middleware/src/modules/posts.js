277efa25bd1cde73e27665a6460dec70bc23d198
ww8007 <wshmin@naver.com>
2021-01-17 01:04:46 +0900

    Add meta:id

--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,32 +1,82 @@
-import * as postsAPI from '../api/posts';
+import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
 import {
   createPromiseThunk,
-  handleAsyncActions,
   reducerUtils,
+  handleAsyncActions,
 } from '../lib/asyncUtils';
 
-const GET_POSTS = 'GET_POSTS';
-const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
-const GET_POSTS_ERROR = 'GET_POSTS_ERROR';
+/* 액션 타입 */
 
+// 포스트 여러개 조회하기
+const GET_POSTS = 'GET_POSTS'; // 요청 시작
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
+
+// 포스트 하나 조회하기
 const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
+// 포스트 비우기
 const CLEAR_POST = 'CLEAR_POST';
 
+// 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST, meta: id });
+  try {
+    const payload = await postsAPI.getPostById(id);
+    dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
+  } catch (e) {
+    dispatch({
+      type: GET_POST_ERROR,
+      payload: e,
+      error: true,
+      meta: id,
+    });
+  }
+};
+
 export const clearPost = () => ({ type: CLEAR_POST });
 
+// initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  post: {},
 };
 
 const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
-const getPostReducer = handleAsyncActions(GET_POST, 'post');
-
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
+        },
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.success(action.payload),
+        },
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.erorr(state.post[id] && state.post[id].data),
+        },
+      };
+    default:
+      return state;
+  }
+};
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
