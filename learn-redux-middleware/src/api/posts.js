0712d8cb6ada49af9f5e30540ac0f697e27eb9ed
ww8007 <wshmin@naver.com>
2021-01-17 13:34:57 +0900

    Renew API with postman

--- learn-redux-middleware/src/api/posts.js
+++ learn-redux-middleware/src/api/posts.js
@@ -1,31 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-// {id, title, body}
-
-const posts = [
-  {
-    id: 1,
-    title: '리덕스 미들웨어 공부',
-    body: '리덕스 미들웨어를 직접 만들어보면 이해 쉬움',
-  },
-  {
-    id: 2,
-    title: 'redux-thunk 사용해봅시다.',
-    body: 'redux-thunk를 사용한 비동기 작업 처리',
-  },
-  {
-    id: 3,
-    title: 'redux-saga',
-    body: 'redux-saga 나중에 사용 비동기 작업 처리',
-  },
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get('http://localhost:4000/posts');
+  return response.data;
 };
-
+// const idSelector = param => param.id;
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 };
