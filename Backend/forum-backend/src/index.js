f84a4fc61611b3a5842ba59ae5662d06a034326c
ww8007 <wshmin@naver.com>
2021-01-19 20:07:13 +0900

    Setting flles

--- Backend/forum-backend/src/index.js
+++ Backend/forum-backend/src/index.js
@@ -1,37 +1,5 @@
-require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-// 비구조 할당을 통해 porcess.env 내부 값에 대한 레퍼런스 만들기
-const { PORT, MOGO_URI } = process.env;
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다
+/* eslint-disable no-global-assign */
 
-mongoose
-  .connect(MOGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-  .then(() => {
-    console.log('Connected to MogoDB');
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-//라우터 설정
-router.use('/api', api.routes());
-
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods);
-
-// 포트가 지정되어있지 안하면 4000사용
-
-const port = PORT || 4000;
-app.listen(port, () => {
-  console.log('Listen to port %d', port);
-});
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
