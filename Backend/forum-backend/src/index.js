cca2da394eb89391d145488afcff5db46838de0f
ww8007 <wshmin@naver.com>
2021-01-19 16:54:51 +0900

    Use Route

--- Backend/forum-backend/src/index.js
+++ Backend/forum-backend/src/index.js
@@ -1,27 +1,17 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; //unthorized
-    return;
-  }
-  await next();
-  console.log('END');
-});
+//라우터 설정
+router.use('/api', api.routes());
 
-app.use((ctx, next) => {
-  console.log('2');
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods);
 
 app.listen(4000, () => {
-  console.log('Listening to port 4000');
+  console.log('Listen port to 4000');
 });
